3b8dcdb3b77c39ae899c144ea2a126ee3fd6e853
Filipe Conde <filipecondep@gmail.com>
2024-03-15 08:57:53 -0300

    chore: creates recent topics component.

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Header from './components/Header.vue'
 import Banner from './components/Banner.vue'
 import DisplaySection from './components/DisplaySection.vue'
+import RecentTopics from './components/RecentTopics.vue'
 </script>
 
 <template>
   <Header />
   <Banner />
   <DisplaySection />
+  <RecentTopics />
   
 </template>
 
---
+++ src/components/RecentTopics.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="recent-topics__container">
+    <h2 class="recent-topics__title">TÓPICOS VISITADOS RECENTEMENTE</h2>
+    <ul class="recent-topics__list">
+      <li class="recent-topics__item">Teste</li>
+      <li class="recent-topics__item">Teste</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
