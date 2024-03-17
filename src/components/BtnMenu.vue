730ebcbb58a662083a5d89fbc1b99be3e1b26279
Filipe Conde <filipecondep@gmail.com>
2024-03-17 10:21:39 -0300

    chore: creates basis for BtnMenu component.

---
+++ src/components/BtnMenu.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <input type="checkbox" id="menu">
+      <label for="menu">
+        <span class="header__menu-icon"></span>
+      </label>
+      <ul class="menu-list">
+        <li class="menu-list__item">TESTE</li>
+        <li class="menu-list__item">TESTE</li>
+        <li class="menu-list__item">TESTE</li>
+        <li class="menu-list__item">TESTE</li>
+      </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {}
+</script>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+
+}
+#menu, .menu-list {
+  display: none;
+}
+
+.header__menu-icon {
+  background-image: url("../icons/Menu.svg");
+  width: 24px;
+  height: 24px;
+  background-repeat: no-repeat;
+  background-position: center;
+  display: inline-block;
+  padding: 0 .2em;
+  margin: 0 .6em;
+}
+
+#menu:checked~label {
+  /* background: var(--dark-blue); */
+  /* margin: 1em 0; */
+  /* background-image: url("../icons/Menu_black_24dp.svg"); */
+}
+
+#menu:checked~.menu-list {
+  display: flex;
+  flex-direction: column;
+  width: auto;
+  height: auto;
+  top: 100%;
+  width: 60vw;
+}
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
     <div class="container">
-      <span class="header__menu-icon"></span>
+      <BtnMenu />
       <img src="../icons/Logo.svg" alt="" class="header__logo">
     </div>
     <div class="container">
@@ -18,28 +18,23 @@
   </header>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import BtnMenu from '@/components/BtnMenu.vue'
+
+export default {
+  components: { BtnMenu }
+}
 
 </script>
 
 <style scoped>
+
 .header {
   background: var(--branco-fundo);
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: .5em;
-}
-
-.header__menu-icon {
-  background-image: url("../icons/Menu.svg");
-  width: 24px;
-  height: 24px;
-  background-repeat: no-repeat;
-  background-position: center;
-  display: inline-block;
-  padding: 0 .2em;
-  margin: 0 .6em;
 }
 
 .container {
